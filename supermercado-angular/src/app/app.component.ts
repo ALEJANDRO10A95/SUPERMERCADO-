7a9af118fef2bfcae40ce103e8d4244c827cb6a7
Carlos Almarcha <extcalmarcha@oesia.com>
2023-08-29 09:46:16 +0200

    añadido componente traducción textos de Alejandro

--- supermercado-angular/src/app/app.component.ts
+++ supermercado-angular/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Cliente } from 'src/interfaces/cliente';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
@@ -10,5 +10,9 @@ import { Cliente } from 'src/interfaces/cliente';
 
 export class AppComponent {
   title = 'Supermercado Angular';
-}
 
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('ES');
+    translate.use('ES');
+  }
+}
