19c9b11bbe1b0c062345ce2df7e00ae14680d42b
Carlos Almarcha <extcalmarcha@oesia.com>
2023-08-31 13:00:06 +0200

    sustituido class por interface

--- supermercado-angular/src/interfaces/compra.ts
+++ supermercado-angular/src/interfaces/compra.ts
@@ -1,16 +1,9 @@
 import { Producto } from './producto.js';
 import { Cliente } from './cliente.js';
 
-export class Compra {
+export interface Compra {
     id: number;
     fecha: Date;
     cliente: Cliente;
     productos: Producto[];
-
-    constructor(id: number, fecha: Date, cliente: Cliente, productos: Producto[]){
-        this.id = id;
-        this.fecha = fecha;
-        this.cliente = cliente;
-        this.productos = productos;
-    }
 }
